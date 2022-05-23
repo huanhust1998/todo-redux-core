d581b8c8654be656371d9dd58c48e89eeb43664f
Huan. Ninh Xuan - CMC Global DU1.19 <nxhuan@cmcglobal.vn>
2022-05-23 15:44:23 +0700

    first commit

---
+++ public/src/redux/actions.js
@@ -0,0 +1,4 @@
+export const addTodoAction = {
+  type: "todoList/addTodo",
+  payload: { id: 4, name: "Learn Java", completed: false, priority: "Low" },
+};
---
+++ public/src/redux/reducer.js
@@ -0,0 +1,29 @@
+const initState = {
+  filters: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  todoList: [
+    { id: 1, name: "Learn Yoga", completed: false, priority: "Medium" },
+    { id: 2, name: "Learn Redux", completed: true, priority: "High" },
+    { id: 3, name: "Learn JavaScript", completed: false, priority: "Low" },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "todoList/addTodo":
+      return {
+        ...state,
+        todoList: [
+          ...state.todoList,
+          { id: 4, name: "Learn Java", completed: false, priority: "Low" },
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
